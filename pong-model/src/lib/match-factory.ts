b9a5ab30ed6721a1886f51ef23d723ae08efc5f9
Maximilian Dresse <max.dresse@gmx.de>
2024-02-04 13:38:31 +0100

    create first box2d objects

--- pong-model/src/lib/match-factory.ts
+++ pong-model/src/lib/match-factory.ts
@@ -1,14 +1,20 @@
 import { Subject } from 'rxjs';
 import { IMatchDef, IObj, MatchFactory } from './types'
+import { b2Vec2, b2World, b2PolygonShape } from '@box2d/core';
 
 export const createMatch: MatchFactory = (def: IMatchDef) => {
     const objectsSub$ = new Subject<Array<IObj>>();
 
     // create box 2d world
-
+    const gravity = new b2Vec2(0.0, -10.0);
+    const world = b2World.Create(gravity);
     // add bodies and tracking IObj for them
-
+    const groundBody = world.CreateBody({ position: { x: 0, y: -10 }});
+    const groundBox = new b2PolygonShape();
+    groundBox.SetAsBox(50.0, 10.0);
+    groundBody.CreateFixture({ shape: groundBox });
     // create main loop
+    
         // for each iteration
             //
 
