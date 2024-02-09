d4ee23d2686d0a4fe9cef7d25b6646f1620dbbcc
Maximilian Dresse <max.dresse@gmx.de>
2024-02-09 20:53:16 +0100

    better constants

---
+++ pong-model/src/lib/input/input-constants.ts
@@ -0,0 +1,4 @@
+export const UNIT_VECTOR_UP = { x: 0, y: 1 };
+export const UNIT_VECTOR_DOWN = { x: 0, y: -1 };
+export const UNIT_VECTOR_LEFT = { x: -1, y: 0 };
+export const UNIT_VECTOR_RIGHT = { x: 1, y: 0 };
--- pong-model/src/lib/input/keyboard-input.ts
+++ pong-model/src/lib/input/keyboard-input.ts
@@ -1,19 +1,20 @@
 import { Observable } from 'rxjs';
 import { InputFactory, Player, Vc2 } from '../types';
 import { MovePlayerIntent, createMovePlayerIntent } from '../intents/player-control-intents';
+import { UNIT_VECTOR_UP, UNIT_VECTOR_DOWN, UNIT_VECTOR_LEFT, UNIT_VECTOR_RIGHT } from './input-constants';
 
-const key2Dir: Record<string, Vc2> = {
-    ArrowUp: { x: 0, y: 1 },
-    ArrowDown: { x: 0, y: -1 },
-    ArrowLeft: { x: -1, y: 0 },
-    ArrowRight: { x: 1, y: 0 },
+const arrowKeys2Dir: Record<string, Vc2> = {
+    ArrowUp: UNIT_VECTOR_UP,
+    ArrowDown: UNIT_VECTOR_DOWN,
+    ArrowLeft: UNIT_VECTOR_LEFT,
+    ArrowRight: UNIT_VECTOR_RIGHT,
 };
 
 export function getKeyboardInput(player: Player): InputFactory {
     return _ctx => new Observable<MovePlayerIntent>(subscriber => {
         const cb: Parameters<(typeof window.addEventListener<'keydown'>)>[1] = ev => {
             const k = ev.key;
-            const d = key2Dir[k];
+            const d = arrowKeys2Dir[k];
             if (!d) {
                 return;
             }
