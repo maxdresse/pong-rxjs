a3550d5523f31f1f550f4da81a4e74ff7e582fde
Maximilian Dresse <max.dresse@gmx.de>
2024-02-11 14:51:31 +0100

    much better keyboad controls

--- pong-model/src/lib/input/keyboard-input.ts
+++ pong-model/src/lib/input/keyboard-input.ts
@@ -4,40 +4,69 @@ import { MovePlayerIntent, createMovePlayerIntent } from '../intents/player-cont
 import { UNIT_VECTOR_UP, UNIT_VECTOR_DOWN, UNIT_VECTOR_LEFT, UNIT_VECTOR_RIGHT } from './input-constants';
 import { combineInputs } from './input-utils';
 
-function getKeyboardInputFromMapping(player: Player, ev2Dir: (ev: KeyboardEvent) => Vc2 | undefined): InputFactory {
-    return _ctx => new Observable<MovePlayerIntent>(subscriber => {
-        const cb: Parameters<(typeof window.addEventListener<'keydown'>)>[1] = ev => {
-            const d = ev2Dir(ev);
-            if (!d) {
+type SymbolicDir = 'u' | 'd' | 'l' | 'r';
+
+const smybolicDir2Dir: Record<SymbolicDir, Vc2> = {
+    u: UNIT_VECTOR_UP,
+    d: UNIT_VECTOR_DOWN,
+    l: UNIT_VECTOR_LEFT,
+    r: UNIT_VECTOR_RIGHT,
+};
+
+function getKeyboardInputFromMapping(player: Player, ev2Dir: (ev: KeyboardEvent) => SymbolicDir | undefined): InputFactory {
+    return ({ onFrame$ }) => new Observable<MovePlayerIntent>(subscriber => {
+        const keyBuf: Array<SymbolicDir> = [];
+        const sub = onFrame$.subscribe(() => {
+            const dir = smybolicDir2Dir[keyBuf[0]];
+            subscriber.next(createMovePlayerIntent({ player, direction: dir}));
+        });
+        const cbDown: Parameters<(typeof window.addEventListener<'keydown'>)>[1] = ev => {
+            const sd = ev2Dir(ev);
+            if (!sd) {
                 return;
             }
-            subscriber.next(createMovePlayerIntent({ player, direction: d}));
+            keyBuf.unshift(sd);
         };
-        window.addEventListener('keydown', cb);
-        subscriber.add(() => window.removeEventListener('keydown', cb));
+        const cbUp: Parameters<(typeof window.addEventListener<'keyup'>)>[1] = ev => {
+            const sd = ev2Dir(ev);
+            if (!sd) {
+                return;
+            }
+            const idx = keyBuf.indexOf(sd);
+            if (idx >= 0) {
+                keyBuf.splice(idx, 1);
+            }
+        };
+        window.addEventListener('keydown', cbDown);
+        window.addEventListener('keyup', cbUp);
+        subscriber.add(() => {
+            sub.unsubscribe();
+            window.removeEventListener('keydown', cbDown);
+            window.removeEventListener('keyup', cbUp);
+        });
     });  
 }
 
-const arrowKeys2Dir: Record<string, Vc2> = {
-    ArrowUp: UNIT_VECTOR_UP,
-    ArrowDown: UNIT_VECTOR_DOWN,
-    ArrowLeft: UNIT_VECTOR_LEFT,
-    ArrowRight: UNIT_VECTOR_RIGHT,
+const arrowKeys2SymbolicDir: Record<string, SymbolicDir> = {
+    ArrowUp: 'u',
+    ArrowDown: 'd',
+    ArrowLeft: 'l',
+    ArrowRight: 'r',
 };
 
 export function getArrowKeyboardInput(player: Player): InputFactory {
-    return getKeyboardInputFromMapping(player, ev => arrowKeys2Dir[ev.key]);
+    return getKeyboardInputFromMapping(player, ev => arrowKeys2SymbolicDir[ev.key]);
 }
 
-const wasdKeys2Dir: Record<string, Vc2> = {
-    KeyW: UNIT_VECTOR_UP,
-    KeyS: UNIT_VECTOR_DOWN,
-    KeyA: UNIT_VECTOR_LEFT,
-    KeyD: UNIT_VECTOR_RIGHT,
+const wasdKeys2SymbolicDir: Record<string, SymbolicDir> = {
+    KeyW: 'u',
+    KeyS: 'd',
+    KeyA: 'l',
+    KeyD: 'r',
 };
 
 function getWASDKeyboardInput(player: Player): InputFactory {
-    return getKeyboardInputFromMapping(player, ev => wasdKeys2Dir[ev.code]);
+    return getKeyboardInputFromMapping(player, ev => wasdKeys2SymbolicDir[ev.code]);
 }
 
 export function getKeyboardInput(): InputFactory {
