916c7fbce45e388d33c4b13864e1846e4c6b41a7
Maximilian Dresse <max.dresse@gmx.de>
2024-02-12 17:11:48 +0100

    correct spelling

--- pong-model/src/lib/effects/goal-effect.ts
+++ pong-model/src/lib/effects/goal-effect.ts
@@ -1,11 +1,11 @@
 import { otherPlayer } from '../player-utils';
 import { GameEffect, Player } from '../types';
 
-export function createGoalEffect(playerWhoseGoalIsHit: Player): GameEffect {
+export function createGoalEffect(playerWhoseGoalWasHit: Player): GameEffect {
     return {
         apply: (gameSituation) => {
             const playerToScore = gameSituation.score.playerToScore;
-            const playerWhoScored = otherPlayer(playerWhoseGoalIsHit);
+            const playerWhoScored = otherPlayer(playerWhoseGoalWasHit);
             playerToScore[playerWhoScored]++;
         }
     };
