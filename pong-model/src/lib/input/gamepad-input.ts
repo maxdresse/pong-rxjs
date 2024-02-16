d2581739704d3bf2b7f2acc1b7209dc7c5cf6e3d
Maximilian Dresse <max.dresse@gmx.de>
2024-02-16 20:06:31 +0100

    working gamepad for two players

--- pong-model/src/lib/input/gamepad-input.ts
+++ pong-model/src/lib/input/gamepad-input.ts
@@ -5,29 +5,24 @@ import { createMovePlayerIntent } from '../intents/player-control-intents';
 export function getGamepadInput(): InputFactory {
     return ({ onFrame$ }) => new Observable(subscriber => {
         let sub: Subscription;
-        let gpIdx = -1;
-        const cb = function(ev: Event) {
-            const e = ev as GamepadEvent;
-            console.log(e.gamepad);
-            if (sub) {
-                sub.unsubscribe();
+        sub = onFrame$.subscribe(() => {
+            // need to actively poll gamepads in navigator
+            // in order to get current state
+            const gpds = navigator.getGamepads().filter(x => !!x) as Array<Gamepad>;
+            if (!gpds?.length) {
+                return;
             }
-            sub = onFrame$.subscribe(() => {
-                gpIdx = e.gamepad.index;
-                // need to actively poll gamepads in navigator,
-                // e.gamepad is just a snapshot
-                const gp = navigator.getGamepads()[gpIdx];
-                if (!gp) {
-                    return;
-                }
-                // scan first 4 axes
+            gpds.forEach((gp, gpIdx) => {
+                // scan first 2 axes
+                const axisCount = 2;
                 const resultBuf: Array<number | undefined> = [undefined, undefined];
                 const stickThreshold = 0.1;
-                gp.axes.slice(0, 8).forEach((v, i) => {
+                gp.axes.slice(0, axisCount * 2).forEach((v, i) => {
                     if (Math.abs(v) < stickThreshold) {
                         return; 
                     }
-                    resultBuf[i % 2] = ((i % 2) ? -1 : 1) * v;
+                    const flipSign = !!(i % 2)
+                    resultBuf[i % 2] = (flipSign ? -1 : 1) * v;
                 });
                 if (resultBuf.some(b => b !== undefined)) {
                     const direction = { x: resultBuf[0] ?? 0, y: resultBuf[1] ?? 0};
@@ -35,11 +30,9 @@ export function getGamepadInput(): InputFactory {
                     subscriber.next(createMovePlayerIntent({ player, direction }));
                 }
             });
-          };
-        // todo: is subscribing to this event listener necessary?
-        window.addEventListener('gamepadconnected', cb);
+            
+        });
         subscriber.add(() => {
-            window.removeEventListener('gamepadconnected', cb);
             if (sub) {
                 sub.unsubscribe();
             }
