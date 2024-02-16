3bb5757a7b61ee74e1dc2bfb5bd75a520a0e2e16
Maximilian Dresse <max.dresse@gmx.de>
2024-02-16 13:47:07 +0100

    reduced rendering

--- pong-model/src/lib/draw-all.ts
+++ pong-model/src/lib/draw-all.ts
@@ -6,8 +6,8 @@ export function drawAll(draw: DebugDraw, params: GameParameters, world: b2World)
     draw.Prepare(0, 0, params.zoomFactor, true); // center, zoom, flipy
     DrawShapes(draw, world);
     DrawJoints(draw, world);
-    DrawAABBs(draw, world);
-    DrawPairs(draw, world);
-    DrawCenterOfMasses(draw, world);
+    // DrawAABBs(draw, world);
+    // DrawPairs(draw, world);
+    // DrawCenterOfMasses(draw, world);
     draw.Finish();
 }
