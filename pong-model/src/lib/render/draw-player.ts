1c44a5ea8054873610c636fe77ceba1bca46e725
Maximilian Dresse <max.dresse@gmx.de>
2024-02-22 06:37:03 +0100

    perfect paddle rendering

--- pong-model/src/lib/render/draw-player.ts
+++ pong-model/src/lib/render/draw-player.ts
@@ -1,12 +1,11 @@
-import { b2Body, b2PolygonShape, b2Shape, b2ShapeType, b2Vec2 } from '@box2d/core';
-import { drawInBodyContext, drawPolygonalFixtures } from './draw-utils';
+import { b2Body, b2PolygonShape, b2ShapeType, b2Vec2 } from '@box2d/core';
+import { drawInBodyContext } from './draw-utils';
 import { IColorScheme, Vc2 } from '../types';
 
 
 export function drawPlayer(ctx: CanvasRenderingContext2D, body: b2Body, cs: IColorScheme): void {
     drawInBodyContext(ctx, body, () => {
         const { player: { fill, stroke } } = cs;
-        drawPolygonalFixtures(body, ctx, { fill, stroke });
         drawPaddle(body, ctx, { fill, stroke });
     });
 }
@@ -29,13 +28,12 @@ export function drawPaddle(body: b2Body, ctx: CanvasRenderingContext2D, colors: 
             console.error('unexpected non-4-vertex shape');
         }
         const vertices = shape.m_vertices;
-        ctx.beginPath();
         ctx.moveTo(vertices[0].x, vertices[0].y);
-        arcBetween(ctx, vertices[0], vertices[1], true);
-        ctx.moveTo(vertices[1].x, vertices[1].y);
-        ctx.lineTo(vertices[1].x, vertices[2].y);
-        arcBetween(ctx, vertices[2], vertices[3], false);
-        ctx.moveTo(vertices[3].x, vertices[3].y);
+        ctx.beginPath();
+        arcBetween(ctx, vertices[0], vertices[1], false, -Math.PI);
+        ctx.lineTo(vertices[2].x, vertices[2].y);
+        arcBetween(ctx, vertices[2], vertices[3], false, 0);
+        ctx.lineTo(vertices[3].x, vertices[3].y);
         ctx.closePath();
         ctx.fillStyle = colors.fill;
         ctx.fill();
@@ -44,8 +42,8 @@ export function drawPaddle(body: b2Body, ctx: CanvasRenderingContext2D, colors: 
     }
 }
 
-function arcBetween(ctx: CanvasRenderingContext2D, a: Vc2, b: Vc2, ccw: boolean): void {
+function arcBetween(ctx: CanvasRenderingContext2D, a: Vc2, b: Vc2, ccw: boolean, startAngle: number): void {
     const middle = new b2Vec2(a.x, a.y).Add(b).Scale(0.5);
     const radius = new b2Vec2(a.x, a.y).Subtract(b).Scale(0.5).Length();
-    ctx.arc(middle.x, middle.y, radius, 0, Math.PI, ccw);
+    ctx.arc(middle.x, middle.y, radius, startAngle, Math.PI + startAngle, ccw);
 }
