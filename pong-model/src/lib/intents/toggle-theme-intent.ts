e40f8228673cbdaaaaab1fdff3b8fe3f335eb4e6
Maximilian Dresse <max.dresse@gmx.de>
2024-02-25 14:16:12 +0100

    begin impl theme switching

---
+++ pong-model/src/lib/intents/toggle-theme-intent.ts
@@ -0,0 +1,16 @@
+import { GameIntent, SomeGameIntent } from '../types';
+
+export const TOGGLE_THEME_INTENT = 17;
+
+export type ToggleThemeIntent = GameIntent<typeof TOGGLE_THEME_INTENT, void>;
+
+export function createToggleThemeIntent(): ToggleThemeIntent {
+    return {
+        type: TOGGLE_THEME_INTENT,
+        payload: undefined
+    };
+}
+
+export function isToggleThemeIntent(intent: SomeGameIntent): intent is ToggleThemeIntent {
+    return intent.type === TOGGLE_THEME_INTENT;
+}
