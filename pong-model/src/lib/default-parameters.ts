4b7c0baa65b23cc116bf62b068e93db051fa0e0d
Maximilian Dresse <max.dresse@gmx.de>
2024-02-27 18:58:05 +0100

    working observable pause

--- pong-model/src/lib/default-parameters.ts
+++ pong-model/src/lib/default-parameters.ts
@@ -3,22 +3,20 @@ import { DEFAULT_POS_ITERATIONS, DEFAULT_VEL_ITERATIONS } from './physical-const
 import { DEFAULT_ZOOM } from './render/render-constants';
 import { DEFAULT_TIME_STEP } from './physical-constants';
 import { EIGHTIES_DARK_SCHEME } from './render/color-schemes/eighties';
-import { createObservableProperty } from './observable-property';
+import { addObservableProperty, createObservableProperty } from './observable-property';
 import { Observable } from 'rxjs';
 
 const GOALS_TO_WIN = 5;
 
 export function getDefaultParameters(): GameParameters {
-    return {
-        ...{
+    return addObservableProperty({
         zoomFactor: DEFAULT_ZOOM,
         positionIterations: DEFAULT_POS_ITERATIONS,
         velocityIterations: DEFAULT_VEL_ITERATIONS,
         timeStep: DEFAULT_TIME_STEP,
         colorScheme: EIGHTIES_DARK_SCHEME,
-        goalsToWin: GOALS_TO_WIN },
-        ...getPausedState(false)
-    };
+        goalsToWin: GOALS_TO_WIN,
+    }, 'paused', 'paused$', false);
 }
 
 function getPausedState(initialVal: boolean): { paused: boolean, paused$: Observable<boolean> } {
--- pong-model/src/lib/observable-property.ts
+++ pong-model/src/lib/observable-property.ts
@@ -15,8 +15,11 @@ export function createObservableProperty<T, K extends string>(propName: K, initi
     ];
 }
 
-export function addObservableProperty<Ob, T, K extends string>(obj: Ob, propName: K, initialVal: T) {
+export function addObservableProperty<Ob extends Record<string, any>, T, K extends string, KK extends string>(obj: Ob, propName: K, propNameForObs: KK, initialVal: T) {
     const v$ = new BehaviorSubject<T>(initialVal);
+    if (!!obj[propName]) {
+        throw Error('cannot overwrite existing property ' + propName);
+    }
     Object.defineProperty(obj, propName, {
         get: function() {
             return v$.getValue();
@@ -25,5 +28,10 @@ export function addObservableProperty<Ob, T, K extends string>(obj: Ob, propName
             v$.next(x);
         }
     });
-    return obj;
+    if (!!obj[propNameForObs]) {
+        throw Error('cannot overwrite existing property ' + propNameForObs);
+    }
+    (obj as { [key in typeof propNameForObs]: Observable<T> } )[propNameForObs] = v$.asObservable();
+    const objWithType = obj as Ob & { [key in typeof propNameForObs]: Observable<T> } & { [key in typeof propName]: T }
+    return objWithType;
 }
