4f851394800ad4a9a0e003043e3fc77161c1df98
Maximilian Dresse <max.dresse@gmx.de>
2024-02-29 15:30:06 +0100

    improve font centering

--- pong-model/src/lib/render/font-constants.ts
+++ pong-model/src/lib/render/font-constants.ts
@@ -1,3 +1,4 @@
 export const USR_MSG_FONT_SIZE = 60;
-export const X_CORRECTION_PER_PT_AND_GLYPH = 0.0002;export const Y_CORRECTION_PER_PT = 0.001;
+export const X_CORRECTION_PER_PT_AND_GLYPH = 0.00017;
+export const Y_CORRECTION_PER_PT = 0.001;
 
