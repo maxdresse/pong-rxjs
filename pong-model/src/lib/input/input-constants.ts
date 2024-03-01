195113b5dea3f92eba3c048c2cdb84e7d3d46813
Maximilian Dresse <max.dresse@gmx.de>
2024-03-01 06:33:33 +0100

    fine grained keyboard controls

--- pong-model/src/lib/input/input-constants.ts
+++ pong-model/src/lib/input/input-constants.ts
@@ -2,3 +2,9 @@ export const UNIT_VECTOR_UP = { x: 0, y: 1 };
 export const UNIT_VECTOR_DOWN = { x: 0, y: -1 };
 export const UNIT_VECTOR_LEFT = { x: -1, y: 0 };
 export const UNIT_VECTOR_RIGHT = { x: 1, y: 0 };
+const invsqr2 = 1 / Math.sqrt(2);
+export const UNIT_VECTOR_UPPER_LEFT = { x: -invsqr2, y: invsqr2 };
+export const UNIT_VECTOR_UPPER_RIGHT = { x: invsqr2, y: invsqr2 };
+export const UNIT_VECTOR_LOWER_RIGHT = { x: invsqr2, y: -invsqr2 };
+export const UNIT_VECTOR_LOWER_LEFT = { x: -invsqr2, y: -invsqr2 };
+
--- pong-model/src/lib/input/keyboard-input.ts
+++ pong-model/src/lib/input/keyboard-input.ts
@@ -1,21 +1,32 @@
 import { Observable, merge } from 'rxjs';
 import { InputFactory, Player, Vc2 } from '../types';
 import { MovePlayerIntent, createMovePlayerIntent } from '../intents/player-control-intents';
-import { UNIT_VECTOR_UP, UNIT_VECTOR_DOWN, UNIT_VECTOR_LEFT, UNIT_VECTOR_RIGHT } from './input-constants';
+import { UNIT_VECTOR_UP, UNIT_VECTOR_DOWN, UNIT_VECTOR_LEFT, UNIT_VECTOR_RIGHT, UNIT_VECTOR_UPPER_LEFT, UNIT_VECTOR_UPPER_RIGHT, UNIT_VECTOR_LOWER_RIGHT, UNIT_VECTOR_LOWER_LEFT } from './input-constants';
 import { combineInputs } from './input-utils';
 
 const enum SymbolicDirection {
-    UP = 1, // need truthy values
-    DOWN,
-    LEFT,
-    RIGHT
+    UP = 1, // need truthy values which are bit-shifts apart
+    DOWN = 2,
+    LEFT = 4,
+    RIGHT = 8
 };
 
-const smybolicDir2VectorDir: Record<SymbolicDirection, Vc2> = {
+const enum CombinedSymbolicDirection {
+    UPPER_LEFT = SymbolicDirection.UP + SymbolicDirection.LEFT,
+    UPPER_RIGHT = SymbolicDirection.UP + SymbolicDirection.RIGHT,
+    LOWER_RIGHT = SymbolicDirection.DOWN + SymbolicDirection.RIGHT,
+    LOWER_LEFT = SymbolicDirection.DOWN + SymbolicDirection.LEFT
+}
+
+const smybolicDir2VectorDir: Record<SymbolicDirection | CombinedSymbolicDirection, Vc2> = {
     [SymbolicDirection.UP]: UNIT_VECTOR_UP,
     [SymbolicDirection.DOWN]: UNIT_VECTOR_DOWN,
     [SymbolicDirection.LEFT]: UNIT_VECTOR_LEFT,
     [SymbolicDirection.RIGHT]: UNIT_VECTOR_RIGHT,
+    [CombinedSymbolicDirection.UPPER_LEFT]: UNIT_VECTOR_UPPER_LEFT,
+    [CombinedSymbolicDirection.UPPER_RIGHT]: UNIT_VECTOR_UPPER_RIGHT,
+    [CombinedSymbolicDirection.LOWER_RIGHT]: UNIT_VECTOR_LOWER_RIGHT,
+    [CombinedSymbolicDirection.LOWER_LEFT]: UNIT_VECTOR_LOWER_LEFT,
 };
 
 function getKeyboardInputFromMapping(player: Player, ev2Dir: (ev: KeyboardEvent) => SymbolicDirection | undefined): InputFactory {
@@ -24,7 +35,7 @@ function getKeyboardInputFromMapping(player: Player, ev2Dir: (ev: KeyboardEvent)
         // trigger a player move intent if a direction is presetn
         const keyBuf: Array<SymbolicDirection> = [];
         const sub = onFrame$.subscribe(() => {
-            const dir = smybolicDir2VectorDir[keyBuf[0]];
+            const dir = symbolicDirToVectorDir(keyBuf);
             if (!dir) {
                 return;
             }
@@ -66,6 +77,15 @@ const arrowKeys2SymbolicDir: Record<string, SymbolicDirection> = {
     ArrowLeft: SymbolicDirection.LEFT,
     ArrowRight: SymbolicDirection.RIGHT,
 };
+
+function symbolicDirToVectorDir(keyBuf: SymbolicDirection[]) {
+    if (keyBuf.length === 1) {
+        return smybolicDir2VectorDir[keyBuf[0]];
+    }
+    // sum has well defined enum value because of bit magic
+    const composedKey: SymbolicDirection | CombinedSymbolicDirection = (keyBuf[0] + keyBuf[1]);
+    return smybolicDir2VectorDir[composedKey];
+}
 
 export function getArrowKeyboardInput(player: Player): InputFactory {
     return getKeyboardInputFromMapping(player, ev => arrowKeys2SymbolicDir[ev.key]);
