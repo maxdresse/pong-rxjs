98a62e3ee199f02f4aa41d9e884f2092c3553b2c
Maximilian Dresse <max.dresse@gmx.de>
2024-03-02 11:25:21 +0100

    revert hack in score update

--- pong-model/src/lib/effects/change-score-effect.ts
+++ pong-model/src/lib/effects/change-score-effect.ts
@@ -5,10 +5,6 @@ type PlayerToScore = GameSituation['score']['playerToScore'];
 export function createChangeScoreEffect(scoreMutationFn: (playerToScore: PlayerToScore) => void): GameEffect {
     return {
         apply: (gameSituation) => {
-            const enabled = gameSituation.ballBody.IsEnabled();
-            if (!enabled) {
-                return;
-            }
             // cpy
             const tmp = JSON.parse(JSON.stringify(gameSituation.score.playerToScore));
             // mutate 
