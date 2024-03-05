3b6b22ef4f225bcd2e4d408f256ef3e8f42342b8
Maximilian Dresse <max.dresse@gmx.de>
2024-03-05 06:55:36 +0100

    custom font

--- pong-model/src/lib/render/draw-overlays.ts
+++ pong-model/src/lib/render/draw-overlays.ts
@@ -1,4 +1,5 @@
 import { GameParameters } from '../types';
+import { FONT_FAMILY } from './font-constants';
 import { Y_CORRECTION_PER_PT } from './font-constants';
 import { X_CORRECTION_PER_PT_AND_GLYPH } from './font-constants';
 import { USR_MSG_FONT_SIZE } from './font-constants';
@@ -17,7 +18,7 @@ export function drawOverlays(ctx: CanvasRenderingContext2D, params: GameParamete
     const yCorrection = Y_CORRECTION_PER_PT * USR_MSG_FONT_SIZE;
     ctx.translate((0.5 - xCorrection) * ctx.canvas.width, (0.5 - yCorrection) * ctx.canvas.height);
     ctx.fillStyle = params.colorScheme.text;
-    ctx.font = `bold ${USR_MSG_FONT_SIZE}pt sans-serif`;
+    ctx.font = `bold ${USR_MSG_FONT_SIZE}pt ${FONT_FAMILY}`;
     ctx.fillText(msg, 0, 0);
     
     ctx.restore();
--- pong-model/src/lib/render/font-constants.ts
+++ pong-model/src/lib/render/font-constants.ts
@@ -1,4 +1,5 @@
 export const USR_MSG_FONT_SIZE = 60;
 export const X_CORRECTION_PER_PT_AND_GLYPH = 0.00017;
 export const Y_CORRECTION_PER_PT = 0.001;
+export const FONT_FAMILY = 'GameFont';
 
