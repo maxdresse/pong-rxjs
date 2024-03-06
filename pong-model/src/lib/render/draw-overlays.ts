7e044f3362a2b464d26324446a94fcfde9f0cf88
Maximilian Dresse <max.dresse@gmx.de>
2024-03-06 16:48:28 +0100

    better text centering

--- pong-model/src/lib/render/draw-overlays.ts
+++ pong-model/src/lib/render/draw-overlays.ts
@@ -1,7 +1,5 @@
 import { GameParameters } from '../types';
 import { FONT_FAMILY } from './font-constants';
-import { Y_CORRECTION_PER_PT } from './font-constants';
-import { X_CORRECTION_PER_PT_AND_GLYPH } from './font-constants';
 import { USR_MSG_FONT_SIZE } from './font-constants';
 
 export function drawOverlays(ctx: CanvasRenderingContext2D, params: GameParameters): void {
@@ -11,14 +9,13 @@ export function drawOverlays(ctx: CanvasRenderingContext2D, params: GameParamete
     const msg = params.userMessage.msg;
     ctx.save();
 
-    // measuring text is expensive
-    // => use a rough heuristic that depends on font size and
-    // text length in order to do the entering
-    const xCorrection = X_CORRECTION_PER_PT_AND_GLYPH * USR_MSG_FONT_SIZE * msg.length;
-    const yCorrection = Y_CORRECTION_PER_PT * USR_MSG_FONT_SIZE;
-    ctx.translate((0.5 - xCorrection) * ctx.canvas.width, (0.5 - yCorrection) * ctx.canvas.height);
-    ctx.fillStyle = params.colorScheme.text;
     ctx.font = `bold ${USR_MSG_FONT_SIZE}pt ${FONT_FAMILY}`;
+    // measuring text is notorious for being expensive
+    // but seems to be Ok in our case
+    const textMetrics = ctx.measureText(msg);
+    const { width: w, actualBoundingBoxAscent: h }  = textMetrics;
+    ctx.translate(0.5 * (ctx.canvas.width - w), 0.5 * (ctx.canvas.height - h));
+    ctx.fillStyle = params.colorScheme.text;
     ctx.fillText(msg, 0, 0);
     
     ctx.restore();
--- pong-model/src/lib/render/font-constants.ts
+++ pong-model/src/lib/render/font-constants.ts
@@ -1,5 +1,3 @@
 export const USR_MSG_FONT_SIZE = 60;
-export const X_CORRECTION_PER_PT_AND_GLYPH = 0.00017;
-export const Y_CORRECTION_PER_PT = 0.001;
 export const FONT_FAMILY = 'GameFont';
 
