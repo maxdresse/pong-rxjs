55307c13ed0d3352d44d2d95bfe0851e8f7f676e
Maximilian Dresse <max.dresse@gmx.de>
2024-03-08 14:28:34 +0100

    perfect score label centering

--- pong-model/src/lib/ui/ui-constants.ts
+++ pong-model/src/lib/ui/ui-constants.ts
@@ -6,3 +6,5 @@ export const PLAYER_STATUS_BORDER_WIDTH = '1px';
 export const PLAYER_STATUS_ALIGNMENT = '0';
 export const PLAYER_BORDER_RADIUS = '8px';
 export const PLAYER_FONT_SIZE = '45pt';
+export const PLAYER_PADDING_BOTTOM = '5px';
+export const PLAYER_PADDING_LEFT = '5px';
--- pong-model/src/lib/ui/ui.ts
+++ pong-model/src/lib/ui/ui.ts
@@ -2,7 +2,7 @@ import { createPauseIntent, createPlayIntent } from '../intents/play-pause-inten
 import { createToggleThemeIntent } from '../intents/toggle-theme-intent';
 import { TEXT_COL_CSS_PROP } from '../render/render-constants';
 import { Player, Score, UIData } from '../types';
-import { PLAYER_BORDER_RADIUS, PLAYER_FONT_SIZE } from './ui-constants';
+import { PLAYER_BORDER_RADIUS, PLAYER_FONT_SIZE, PLAYER_PADDING_BOTTOM, PLAYER_PADDING_LEFT } from './ui-constants';
 import { PLAYER_STATUS_W, PLAYER_STATUS_H, PLAYER_STATUS_LH, PLAYER_STATUS_BORDER_WIDTH, PLAYER_STATUS_ALIGNMENT } from './ui-constants';
 
 const UI_KEY_EVENT_TO_HANDLER: { [key: string]: (uiData: UIData) => void } = {
@@ -71,6 +71,9 @@ function initPlayerScoreUIElement(stl: CSSStyleDeclaration, textcol: string) {
     stl.bottom = PLAYER_STATUS_ALIGNMENT;
     stl.borderRadius = PLAYER_BORDER_RADIUS;
     stl.fontSize = PLAYER_FONT_SIZE;
+    // padding is needed for perfect label centering
+    stl.paddingBottom = PLAYER_PADDING_BOTTOM;
+    stl.paddingLeft = PLAYER_PADDING_LEFT;
 }
 
 function togglePlayPause(uiData: UIData) {
