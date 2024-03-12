c9ba9cb32cf795e8433ea22055df762b20b5dd3e
Maximilian Dresse <max.dresse@gmx.de>
2024-03-12 15:44:34 +0100

    improve stats logging

--- pong-model/src/lib/stats/stats-entries.ts
+++ pong-model/src/lib/stats/stats-entries.ts
@@ -1,11 +1,44 @@
 import { GameSituation, Player, ValueType } from '../types';
 
 export const STATS_ENTRIES: Array<{ attrId: string; label: string; getValue: (gs: GameSituation) => ValueType; }> = [
-    {
-        attrId: 'p1x',
-        label: 'P1 Velocity x',
-        getValue: (gameSituation: GameSituation) => {
-            return gameSituation.playerBodies[Player.PLAYER1].GetLinearVelocity().x;
-        }
-    }
+    getVelocity(Player.PLAYER1, 'x'),
+    getVelocity(Player.PLAYER1, 'y'),
+    getVelocity(Player.PLAYER2, 'x'),
+    getVelocity(Player.PLAYER2, 'y'),
+    getPosition(Player.PLAYER1, 'x'),
+    getPosition(Player.PLAYER1, 'y'),
+    getPosition(Player.PLAYER2, 'x'),
+    getPosition(Player.PLAYER2, 'y'),
 ];
+
+function getVelocity(player: Player, xOrY: 'x' | 'y'): { attrId: string; label: string; getValue: (gs: GameSituation) => ValueType; } {
+    return {
+        attrId: getPlayerAttributeId(player, xOrY, 'v'),
+        label: getPlayerLabel(player, xOrY, 'Velocity'),
+        getValue: (gameSituation: GameSituation) => gameSituation
+            .playerBodies[player]
+            .GetLinearVelocity()[xOrY]
+    };
+}
+
+function getPosition(player: Player, xOrY: 'x' | 'y'): { attrId: string; label: string; getValue: (gs: GameSituation) => ValueType; } {
+    return {
+        attrId: getPlayerAttributeId(player, xOrY, 'p'),
+        label: getPlayerLabel(player, xOrY, 'Position'),
+        getValue: (gameSituation: GameSituation) => gameSituation
+            .playerBodies[player]
+            .GetPosition()[xOrY]
+    };
+}
+
+function getPlayerLabel(player: Player, xOrY: string, infix: string): string {
+    return 'P' + plStr(player) + infix + ' ' + xOrY;
+}
+
+function getPlayerAttributeId(player: Player, xOrY: string, infix: string): string {
+    return 'p' + infix + plStr(player) + xOrY;
+}
+
+function plStr(player: Player): string {
+    return '' + (player + 1);
+}
