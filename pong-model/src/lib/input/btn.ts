270aae55706568f1d38c4766b88c9f41cc6ae313
Maximilian Dresse <max.dresse@gmx.de>
2024-03-16 16:42:07 +0100

    finish combo detector impl

--- pong-model/src/lib/input/btn.ts
+++ pong-model/src/lib/input/btn.ts
@@ -14,22 +14,30 @@ export const enum SymbolicButton {
 // don't have a stronger type for the time being
 export type SymbolicButtonCombination = number;
 
-function onCombo(sequence: Array<SymbolicButtonCombination>): (src$: Observable<SymbolicButtonCombination>) => Observable<void> {
+function onCombo(sequence: Array<SymbolicButtonCombination>, stepTimeout: number): (src$: Observable<SymbolicButtonCombination>) => Observable<void> {
     let currentIdx = 0;
     const lastIndex = sequence.length - 1;
+    let to: ReturnType<typeof setTimeout>;
+    const reset = () => {
+        currentIdx = 0;
+    }
     return src$ => src$.pipe(
         filter(combination => {
+            to = setTimeout(reset, stepTimeout);
             if (currentIdx > lastIndex) {
+                clearTimeout(to);
                 return true;
             }
             if (sequence[currentIdx] === combination) {
                 // match
                 currentIdx++;
+                clearTimeout(to);
                 return currentIdx > lastIndex;
             } else {
                 // no match => reset
-                currentIdx = 0;
+                reset();
             }
+            clearTimeout(to);
             return false;
         }),
         map(_ => undefined)
