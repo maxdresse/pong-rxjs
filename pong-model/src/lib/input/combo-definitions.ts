e12dc5b1004c00a01daff50f834039fd8eecea18
Maximilian Dresse <max.dresse@gmx.de>
2024-03-19 15:16:47 +0100

    map combo to intent

--- pong-model/src/lib/input/combo-definitions.ts
+++ pong-model/src/lib/input/combo-definitions.ts
@@ -1,12 +1,14 @@
 import { HandicapType } from '../types';
 import { SymbolicButton, SymbolicButtonCombination } from './btn';
 
+const dc = defineCombination;
+
 export const COMBO_DEFINITIONS:  Array<[HandicapType, Array<SymbolicButtonCombination>]> = 
     [
-        [HandicapType.OPPONENT_ENLARGED, []]
+        [HandicapType.OPPONENT_ENLARGED, [dc('xyab')]]
     ];
 
-const charToSym = {
+const charToSym: Record<string, SymbolicButton> = {
     a: SymbolicButton.A,
     b: SymbolicButton.B,
     x: SymbolicButton.X,
@@ -16,9 +18,13 @@ const charToSym = {
 };
 
 function defineCombination(btnString: string): SymbolicButtonCombination {
-    const result: SymbolicButtonCombination;
+    let result: SymbolicButtonCombination = 0;
     for (const char of btnString) {
         const sym = charToSym[char];
+        if (sym == undefined) {
+            throw Error('illegal sequence');
+        }
+        result += sym;
    }
    return result;
 }
--- pong-model/src/lib/intents/handicap-intent.ts
+++ pong-model/src/lib/intents/handicap-intent.ts
@@ -2,12 +2,20 @@ import { GameIntent, HandicapType, Player, SomeGameIntent } from '../types';
 
 export const HANDICAP_INTENT = 8;
 
-export type HandicapIntent = GameIntent<typeof HANDICAP_INTENT, Player>;
+export interface HandicapPayload {
+    handicappedPlayer: Player;
+    type: HandicapType;
+}
+
+export type HandicapIntent = GameIntent<typeof HANDICAP_INTENT, HandicapPayload>;
 
 export function createHandicapIntent(handicappedPlayer: Player, type: HandicapType): HandicapIntent {
     return {
         type: HANDICAP_INTENT,
-        payload: handicappedPlayer
+        payload: {
+            handicappedPlayer,
+            type
+        }
     };
 }
 
