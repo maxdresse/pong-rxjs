a1430d1146ede105cf2d2568a9cf1dd289e398f3
Maximilian Dresse <max.dresse@gmx.de>
2024-03-19 05:48:57 +0100

    wip combo definitions

--- pong-model/src/lib/input/combo-definitions.ts
+++ pong-model/src/lib/input/combo-definitions.ts
@@ -1,6 +1,15 @@
 import { HandicapType } from '../types';
-import { SymbolicButtonCombination } from './btn';
+import { SymbolicButton, SymbolicButtonCombination } from './btn';
 
-export const COMBO_DEFINITIONS: { [key in HandicapType]: Array<SymbolicButtonCombination> } = {
-    [HandicapType.OPPONENT_ENLARGED]
-};
+export const COMBO_DEFINITIONS:  Array<[HandicapType, Array<SymbolicButtonCombination>]> = 
+    [
+        [HandicapType.OPPONENT_ENLARGED, []]
+    ];
+
+function defineCombination(btnString: string): SymbolicButtonCombination {
+    const result: SymbolicButtonCombination;
+    for (const char of btnString) {
+
+   }
+   return result;
+}
