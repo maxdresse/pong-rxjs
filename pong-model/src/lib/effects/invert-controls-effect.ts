56192c629c33c957d1e7fe039d6ce38293e93c42
Maximilian Dresse <max.dresse@gmx.de>
2024-03-21 16:02:53 +0100

    wip

--- pong-model/src/lib/effects/invert-controls-effect.ts
+++ pong-model/src/lib/effects/invert-controls-effect.ts
@@ -1,10 +1,19 @@
 import { DEFAUL_MUTATION_DURATION } from '../physical-constants';
+import { getMsgType } from '../render/user-message-utils';
 import { GameEffect, GameSituation, Player } from '../types';
 
 export function createInvertControlsEffect(player: Player, duration = DEFAUL_MUTATION_DURATION):  GameEffect {
     return {
         apply: ({ params }: GameSituation) => {
             params.control[player].inverted = true;
+            params.userMessage = {
+                type: getMsgType(player),
+                flashing: true,
+                msg: 'Invert Controls'
+            }
+            setTimeout(() => {
+                params.userMessage = undefined;
+            }, 3000);
             setTimeout(() => {
                 params.control[player].inverted = false;
             }, duration)
--- pong-model/src/lib/render/user-message-utils.ts
+++ pong-model/src/lib/render/user-message-utils.ts
@@ -1,5 +1,9 @@
-import { Player } from '../types';
+import { Player, UserMessageType } from '../types';
 
 export function getPlayerDisplayName(player: Player): string {
     return `Player ${player + 1}`;
+}
+
+export function getMsgType(player: Player): UserMessageType {
+    return player === Player.PLAYER1 ? UserMessageType.P1 : UserMessageType.P2;
 }
