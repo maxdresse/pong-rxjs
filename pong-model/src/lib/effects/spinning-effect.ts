083f0882269f24b4050b04fc0182f71311f4f758
Maximilian Dresse <max.dresse@gmx.de>
2024-03-23 20:10:27 +0100

    wip spinning

---
+++ pong-model/src/lib/effects/spinning-effect.ts
@@ -0,0 +1,31 @@
+import { GameEffect, Player } from '../types';
+import { DEFAUL_MUTATION_DURATION } from '../physical-constants';
+import { showFlashingMsgToPlayer } from '../render/user-message-utils';
+
+export function createSpinningEffect(player: Player, duration = DEFAUL_MUTATION_DURATION): GameEffect {
+    return {
+        apply: ({ playerBodies, params }) => {
+            const body = playerBodies[player];
+            if (!body) {
+                return;
+            }
+            body.SetFixedRotation(false);
+            body.SetAngularDamping(0);
+            let cancelled = false;
+            const applyTorq = () => {
+                body.ApplyTorque(0.01);
+                if (cancelled) {
+                    return;
+                }
+                requestAnimationFrame(applyTorq);
+            }
+            requestAnimationFrame(applyTorq);
+            showFlashingMsgToPlayer('Spinning', player, params);
+            setTimeout(() => {
+                cancelled = true;
+                body.SetFixedRotation(true);
+                body.SetAngle(0);
+            }, duration)
+        }
+    };
+}
